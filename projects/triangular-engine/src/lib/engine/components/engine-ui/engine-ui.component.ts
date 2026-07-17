bacb8ec51aa319f5460f2913d6be0a840e6241ba
Bruno Kertesz <daxur.studios@gmail.com>
2026-07-17 23:12:34 +0100

    feat: enhance engine UI with overlay lanes and conditional FPS stats display

--- projects/triangular-engine/src/lib/engine/components/engine-ui/engine-ui.component.ts
+++ projects/triangular-engine/src/lib/engine/components/engine-ui/engine-ui.component.ts
@@ -5,11 +5,11 @@ import { EngineService } from '../../services';
 import { EngineStatsComponent } from './engine-stats/engine-stats.component';
 import { IUserInterfaceOptions } from '../../models';
 import { SceneTreeComponent } from '../../features/scene-tree/scene-tree.component';
-import { PortalOutletComponent } from '@daxur-studios/portal-layout';
+import { PortalOutletComponent, PortalTargetDirective } from '@daxur-studios/portal-layout';
 
 @Component({
   selector: 'engine-ui',
-  imports: [CommonModule, EngineStatsComponent, SceneTreeComponent, PortalOutletComponent],
+  imports: [CommonModule, EngineStatsComponent, SceneTreeComponent, PortalOutletComponent, PortalTargetDirective],
   templateUrl: './engine-ui.component.html',
   styleUrl: './engine-ui.component.scss'
 })
