63efb60811065236091a5a1b3961b7a5ca206ed5
Bruno Kertesz <daxur.studios@gmail.com>
2026-08-15 12:33:17 +0100

    fix(light): update shadow follow initialization to use postTick observable

--- projects/triangular-engine/src/lib/engine/components/light/directional-light.component.ts
+++ projects/triangular-engine/src/lib/engine/components/light/directional-light.component.ts
@@ -130,7 +130,7 @@ export class DirectionalLightComponent extends LightComponent {
   }
 
   #initShadowFollow() {
-    this.engineService.tick$
+    this.engineService.postTick$
       .pipe(takeUntil(this.destroy$))
       .subscribe(() => {
         const followed = this.shadowFollow();
